5ab9b472000e2b78efaf4216322771e37c5e466e
Arpit Dubey <arpit@gamezop.co>
2020-01-12 22:33:47 +0000

    todo genserver as mix project

---
+++ todo/lib/server.ex
@@ -0,0 +1,35 @@
+defmodule Todo.Server do
+  use GenServer
+
+  def start do
+    GenServer.start(__MODULE__, nil,name: __MODULE__)
+  end
+
+  def init(_) do
+    {:ok, Todo.List.new()}
+  end
+
+  def add(id,task) do
+    GenServer.cast(__MODULE__, {:add,id,task})
+  end
+
+  def delete(id) do
+    GenServer.cast(__MODULE__, {:delete,id})
+  end
+
+  def get(id) do
+    GenServer.call(__MODULE__, {:get,id})
+  end
+
+  def handle_cast({:add, key, value}, state) do
+    {:noreply, Todo.List.add(state, key, value)}
+  end
+
+  def handle_cast({:delete, key}, state) do
+    {:noreply, Todo.List.delete(state, key)}
+  end
+
+  def handle_call({:get, key}, _, state) do
+    {:reply, Todo.List.get(state, key), state}
+  end
+end
