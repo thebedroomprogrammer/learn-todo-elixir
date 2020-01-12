761769b8601ae648331f5efdf09d519a4106e9c4
Arpit Dubey <arpit@gamezop.co>
2020-01-12 20:04:44 +0000

    crud todo

--- todo_server.ex
+++ todo_server.ex
@@ -3,11 +3,16 @@ defmodule TodoList do
     %{}
   end
 
-  def add_entry(todo_list,date,task) do
-    Map.update(todo_list, date, [task], fn tasks -> [task | tasks] end)
+  def add_entry(todo_list,id,task) do
+    Map.update(todo_list, id, [task], fn tasks -> [task | tasks] end)
   end
 
-  def entries(todo_list,date) do
-    Map.get(todo_list, date, [])
+  def entries(todo_list,id) do
+    Map.get(todo_list, id, [])
   end
+
+  def delete(todo_list,id) do
+    Map.delete(todo_list, id)
+  end
+
 end
